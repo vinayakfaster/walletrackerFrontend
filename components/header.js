ca873614f12ba16f21bf8a059813a6a79712ec72
vinayak shukla <65018895+vinayakfaster@users.noreply.github.com>
2024-02-04 03:22:02 +0530

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
